5ae7ea297fb2506709d9e1f81c0643177be7dbdd
Rui <77292122+Rui-1004@users.noreply.github.com>
2024-10-07 18:20:21 +0100

    Refactor code to use Class

--- library.js
+++ library.js
@@ -2,32 +2,88 @@ const myLibrary = [];
 let bookIndex = 0;
 let id = 0;
 
-function Book(name, author, pages) {
-  this.id = id++;
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = false;
-}
-
-Book.prototype.toggleRead = function () {
-  if(this.read == true) {
+class Book {
+  constructor (name, author, pages) {
+    this.id = id++;
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
     this.read = false;
   }
-  else {
-    this.read = true;
+
+  toggleRead() {
+    if(this.read == true) {
+      this.read = false;
+    }
+    else {
+      this.read = true;
+    }
+  }
+
+  static displayBooks() {
+    for(const book of myLibrary) {
+      createBook(book);
+    }
+  }
+  
+  static createBook(book) {
+    const container = document.querySelector(".book-container");
+  
+    const div = document.createElement("div");
+    const pBook = document.createElement("p");
+    const pAuthor = document.createElement("p");
+    const pPages = document.createElement("p");
+    const pRead = document.createElement("p");
+    const deleteButton = document.createElement("button");
+    const readButton = document.createElement("button");
+    
+    pRead.classList.add("read-text");
+    div.classList.add("book-content");
+    deleteButton.classList.add("delete");
+    readButton.classList.add("read")
+    deleteButton.addEventListener("click", () => {
+      const bookContent = document.querySelector(`[data-index="${book.id}"]`);
+
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      console.log(myLibrary);
+      bookContent.remove();
+    });
+    readButton.addEventListener("click", () => {
+      const bookContent = document.querySelector(`[data-index="${book.id}"]`);;
+      const pRead = bookContent.querySelector(".read-text");
+    
+      book.toggleRead();
+      console.log(book);
+      pRead.textContent = book.read == true ? "read" : "not read";
+    });
+  
+    div.dataset.index = bookIndex;
+          
+    pBook.textContent = book.name;
+    pAuthor.textContent = book.author;
+    pPages.textContent = book.pages;
+    pRead.textContent = "not read";
+    deleteButton.textContent = "Delete";
+    readButton.textContent = "Toggle Read";
+          
+    div.append(pBook, pAuthor, pPages, pRead, deleteButton, readButton);
+          
+    container.appendChild(div);
+    bookIndex++;
   }
 }
 
-function displayBooks() {
-  for(const book of myLibrary) {
-    createBook(book);
-  }
-}
+Book.displayBooks();
 
-function addBookToLibrary() {
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", () => {
   event.preventDefault();
-
+  
   const bookName = document.getElementById("book-name").value;
   const authorName = document.getElementById("author-name").value;
   const pageNumber = document.getElementById("pages").value;
@@ -36,70 +92,8 @@ function addBookToLibrary() {
 
   myLibrary.push(book);
   console.log(myLibrary);
-  createBook(book);
-}
-
-function createBook(book) {
-  const container = document.querySelector(".book-container");
-
-  const div = document.createElement("div");
-  const pBook = document.createElement("p");
-  const pAuthor = document.createElement("p");
-  const pPages = document.createElement("p");
-  const pRead = document.createElement("p");
-  const deleteButton = document.createElement("button");
-  const readButton = document.createElement("button");
-        
-  div.classList.add("book-content");
-  deleteButton.classList.add("delete");
-  readButton.classList.add("read")
-  deleteButton.setAttribute("onclick", "removeBook()");
-  readButton.setAttribute("onclick", "readBook()");
-
-  div.dataset.index = bookIndex;
-        
-  pBook.textContent = book.name;
-  pAuthor.textContent = book.author;
-  pPages.textContent = book.pages;
-  pRead.textContent = book.read == true ? "read" : "not read";
-  deleteButton.textContent = "Delete";
-  readButton.textContent = "Toggle Read";
-        
-  div.append(pBook, pAuthor, pPages, pRead, deleteButton, readButton);
-        
-  container.appendChild(div);
-  bookIndex++;
-}
-
-function removeBook() {
-  const button = document.querySelector(".delete");
-  const bookContent = button.parentNode;
-
-  for(const book of myLibrary) {
-    if(book.id == bookContent.dataset.index) {
-      myLibrary.splice(myLibrary.indexOf(book), 1);
-      console.log(myLibrary);
-    }
-  }
-  bookContent.remove();
-}
-
-function readBook() {
-  const readBtn = document.querySelector(".read");
-  const bookContent = readBtn.parentNode;
-
-  for(const book of myLibrary) {
-    if(book.id == bookContent.dataset.index) {
-      book.toggleRead();
-    }
-  }
-}
-
-displayBooks();
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+  Book.createBook(book);
+});
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
